fe793d93fd8fa052ca451821629f5002c4ba9b1a
wangpingo <350316275@qq.com>
2016-11-28 20:18:16 +0800

    更新代码

--- js面向对象编程/继承.js
+++ js面向对象编程/继承.js
@@ -6,13 +6,13 @@
 //     this.species='动物';
 //     // this.args=args;
 // }
-function Animal() {
-}
-Animal.prototype.species="动物";
-function Cat(name,color) {
-    this.name=name;
-    this.color=color;
-}
+// function Animal() {
+// }
+// Animal.prototype.species="动物";
+// function Cat(name,color) {
+//     this.name=name;
+//     this.color=color;
+// }
 //一。构造函数绑定
 // function Cat(name,color) {
 //     Animal.call(this,"4");
@@ -56,14 +56,60 @@ function Cat(name,color) {
 //     Child.uber = Parent.prototype;
 // }
 //拷贝继承
-function extend2(Child, Parent) {
-    var p = Parent.prototype;
-    var c = Child.prototype;
+// function extend2(Child, Parent) {
+//     var p = Parent.prototype;
+//     var c = Child.prototype;
+//     for (var i in p) {
+//         c[i] = p[i];
+//     }
+//     // c.uber = p;
+// }
+// extend2(Cat, Animal);
+// var cat1 = new Cat("大毛","黄色");
+// console.log(cat1.species); // 动物
+//非构造函数继承
+//一，object方法
+var Chinese={
+    nation:'中国'
+};
+// var Doctor={
+//     carrer:'医生'
+// };
+// function object(o) {
+//     function F() {
+//     }
+//     F.prototype=o;
+//     return new F();
+// }
+// var Doctor=object(Chinese);
+// Doctor.carrer='医生';
+// console.log(Doctor.carrer)
+//浅拷贝
+function extendCopy(p) {
+    var c = {};
     for (var i in p) {
         c[i] = p[i];
     }
-    // c.uber = p;
+    c.uber = p;
+    return c;
 }
-extend2(Cat, Animal);
-var cat1 = new Cat("大毛","黄色");
-console.log(cat1.species); // 动物
+var Doctor = extendCopy(Chinese);
+Doctor.career = '医生';
+console.log(Doctor.nation); // 中国
+//四、深拷贝
+//所谓"深拷贝"，就是能够实现真正意义上的数组和对象的拷
+//贝。它的实现并不难，只要递归调用"浅拷贝"就行了。
+function deepCopy(p, c) {
+    var c = c || {};
+    for (var i in p) {
+        if (typeof p[i] === 'object') {
+            c[i] = (p[i].constructor === Array) ? [] : {};
+            deepCopy(p[i], c[i]);
+        } else {
+            c[i] = p[i];
+        }
+    }
+    return c;
+}
+var a=[];
+console.log(typeof a);

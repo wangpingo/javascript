5d6efbbba65716fd6b61b76e25d0aa86efb90bcc
wangpingo <350316275@qq.com>
2016-11-29 23:35:58 +0800

    更新代码

---
+++ kata/groupCheck.js
@@ -0,0 +1,35 @@
+/**
+ * Created by 35031 on 2016/11/29.
+ */
+function groupCheck(s) {
+    var arr = s.split('');
+    if (arr.length % 2 == 0) {
+        for (var i = 0; i < arr.length/2; i++) {
+            if((arr.length - 1 - i)%2==0)return false;
+            switch (arr[i]) {
+                case '(':
+                    if (arr[(arr.length - 1 - i) != ")"])
+                        return false;
+                    break;
+                case '{' :
+                    if (arr[(arr.length - 1 - i) != "}"])
+                        return false;
+                    break;
+                case '[' :
+                    if (arr[(arr.length - 1 - i) != "]"])
+                        return false;
+                    break;
+                default:
+                    return false;
+                    break;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
+
+}
+console.log(groupCheck('()'));
+console.log(groupCheck('{(})'));
+console.log(groupCheck('[])'));
--- kata/triangleType.js
+++ kata/triangleType.js
@@ -6,16 +6,16 @@ function triangleType(a, b, c) {
     triangleArr.sort(function (a, b) {
         return a - b;
     });
-    if (triangleArr[2] > triangleArr[1] + triangleArr[0]) {
+    if (triangleArr[2] < triangleArr[1] + triangleArr[0]) {
         var fAngle=triangleArr[0]*triangleArr[0]+triangleArr[1]*triangleArr[1]-triangleArr[2]*triangleArr[2];
         if (fAngle>0)return 1;
-        if (fAngle=0)return 2;
+        if (fAngle==0)return 2;
         if (fAngle<0)return 3;
     }
     return 0;
 }
-triangleType(9, 3, 2);
-triangleType(2, 4, 6);
-triangleType(8, 5, 7);
-triangleType(3, 4, 5);
-triangleType(7, 12, 8);
+console.log(triangleType(9, 3, 2));
+console.log(triangleType(2, 4, 6));
+console.log(triangleType(8, 5, 7));
+console.log(triangleType(3, 4, 5));
+console.log(triangleType(7, 12, 8));

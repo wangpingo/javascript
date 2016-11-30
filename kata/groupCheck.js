259ad381ff319506d484d349f6b889f85a2fd4e7
wangpingo <350316275@qq.com>
2016-11-30 19:22:23 +0800

    更新代码

--- kata/groupCheck.js
+++ kata/groupCheck.js
@@ -1,42 +1,106 @@
 /**
  * Created by 35031 on 2016/11/29.
  */
-function groupCheck(s) {
-    var arr = s.split('');
-    if (arr.length % 2 == 0) {
-        for (var i = 0; i < arr.length/2; i++) {
 
-            if((arr.length - 2 - i-i)%2!=0) return false;
-
-            switch (arr[i]) {
-                case '(':
-                    if (arr[(arr.length - 1 - i)] != ")")
-                        return false;
-                    break;
-                case '{' :
-                    if (arr[(arr.length - 1 - i)] != "}")
-                        return false;
-                    break;
-                case '[' :
-                    if (arr[(arr.length - 1 - i)] != "]")
-                        return false;
-                    break;
-                default:
-                    return false;
-                    break;
+function groupCheck(str,a) {
+    var moudleJson = {
+        '}': '{',
+        ')': '(',
+        ']': '['
+    };
+    var teamSaveArray = [];
+    for (var i = 0; i < str.length; i++) {
+        for (var key in moudleJson) {
+            if (str[i] == key || str[i] == moudleJson[key]) {
+                teamSaveArray.push(str[i]);
             }
         }
-        return true;
-    } else {
-        return false;
     }
-
+    if (teamSaveArray.length) {
+        if ((teamSaveArray.length % 2) != 0) {
+            return false==a;
+        } else {
+            for (var j = 0; j < teamSaveArray.length; j++) {
+                if (j > 0) {
+                    if (moudleJson[teamSaveArray[j]] == teamSaveArray[j - 1]) {
+                        teamSaveArray.splice(j - 1, 2);
+                        j = 0;
+                    }
+                }
+            }
+        }
+        if (teamSaveArray.length) {
+            return false==a;
+        } else {
+            return true==a;
+        }
+    } else {
+        return true==a;
+    }
 }
-// console.log(groupCheck('()'));
-// console.log(groupCheck('{(})'));
-// console.log(groupCheck('[])'));
-// console.log(groupCheck('({})'));
-console.log(groupCheck('[[]()]'));
-// console.log(groupCheck('[{()}]'));
-
-
+    console.log(groupCheck("({", false))
+    console.log(groupCheck("()", true))
+    console.log(groupCheck("([", false))
+    console.log(groupCheck("[{", false))
+    console.log(groupCheck("((", false))
+    console.log(groupCheck("{{", false))
+    console.log(groupCheck("[[", false))
+    console.log(groupCheck("))", false))
+    console.log(groupCheck("}}", false))
+    console.log(groupCheck("]]", false))
+    console.log(groupCheck("{", false))
+    console.log(groupCheck("(", false))
+    console.log(groupCheck("[", false))
+    console.log(groupCheck("}", false))
+    console.log(groupCheck(")", false))
+    console.log(groupCheck("]", false))
+    console.log(groupCheck("a{b", false))
+    console.log(groupCheck(" ( ", false))
+    console.log(groupCheck("a[ ", false))
+    console.log(groupCheck("a } b", false))
+    console.log(groupCheck("a)b", false))
+    console.log(groupCheck("a]", false))
+    console.log(groupCheck("()(a", false))
+    console.log(groupCheck("(()", false))
+    console.log(groupCheck("{}{", false))
+    console.log(groupCheck("{{}", false))
+    console.log(groupCheck("()(", false))
+    console.log(groupCheck("[[]", false))
+    console.log(groupCheck("{{}}", false))
+    console.log(groupCheck("[[]]", false))
+    console.log(groupCheck("(())", false))
+    console.log(groupCheck("{}[]()", true))
+    console.log(groupCheck("{[]}()", true))
+    console.log(groupCheck("{[]()}", true))
+    console.log(groupCheck("({}[])", true))
+    console.log(groupCheck("{[}]()", false))
+    console.log(groupCheck("{[](})", false))
+    console.log(groupCheck("{[}]()", false))
+    console.log(groupCheck("{(}[])", false))
+    console.log(groupCheck("{}[](){", false))
+    console.log(groupCheck("{}[]()[", false))
+    console.log(groupCheck("{}[]()(", false))
+    console.log(groupCheck("){}[]()", false))
+    console.log(groupCheck("}{}[]()", false))
+    console.log(groupCheck("]{}[]()", false))
+    console.log(groupCheck("{a}[b](c)", true))
+    console.log(groupCheck("{[a]}(b)", true))
+    console.log(groupCheck("{[a](b)}", true))
+    console.log(groupCheck("({a}b[c])", true))
+    console.log(groupCheck("{[a}b]c()", false))
+    console.log(groupCheck("{[a]b(c})", false))
+    console.log(groupCheck("{[a}b]c()", false))
+    console.log(groupCheck("{(a}b[c])", false))
+    console.log(groupCheck("{a}b[c]d(e))f{", false))
+    console.log(groupCheck("{a}[b](c)[", false))
+    console.log(groupCheck("{}a[]b()c(", false))
+    console.log(groupCheck("(()) [[]] {{}}", false))
+    console.log(groupCheck("({()}) [([])] {[{}]}", true))
+    console.log(groupCheck("(())", false))
+    console.log(groupCheck("({()})", true))
+    console.log(groupCheck("[[]]", false))
+    console.log(groupCheck("[([])]", true))
+    console.log(groupCheck("{{}}", false))
+    console.log(groupCheck("{[{}]}", true))
+    console.log(groupCheck("", true))
+    console.log(groupCheck("Hello World?", true))

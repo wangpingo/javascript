57e9fe129f4264d1c4642da8b0d05f596a197177
wangpingo <350316275@qq.com>
2016-11-30 21:31:19 +0800

    更新代码

--- kata/UniqueInOrder.js
+++ kata/UniqueInOrder.js
@@ -3,32 +3,53 @@
  */
 //Test.assertSimilar(uniqueInOrder('AAAABBBCCDAABBB'),
 // ['A','B','C','D','A','B'])
+// function uniqueInOrder(str) {
+//     var aStr = str.split('');
+//     var newStr=[];
+//     var compare;
+//     var iCompare=0;
+//     var istart=0;
+//     compare = aStr[0];
+//     newStr.push(compare);
+//     do {
+//         toCompare(istart,aStr)
+//     }while (iCompare!=aStr.length);
+//     function toCompare(start,aStr) {
+//         for (var i=start;i<aStr.length;i++){
+//             iCompare++;  //4,
+//             if (compare==aStr[i]){
+//                 continue;
+//             }
+//             compare=aStr[i];//B
+//             newStr.push(compare);//['A','B']
+//             break;
+//         }
+//         istart=i+1;
+//     }
+//    return newStr;
+// };
 function uniqueInOrder(str) {
-    var aStr = str.split('');
-    var newStr=[];
-    var compare;
-    var iCompare=0;
-    var istart=0;
-    compare = aStr[0];
-    newStr.push(compare);
-    do {
-        toCompare(istart,aStr)
-    }while (iCompare!=aStr.length);
-    function toCompare(start,aStr) {
-        for (var i=start;i<aStr.length;i++){
-            iCompare++;  //4,
-            if (compare==aStr[i]){
-                continue;
+    if (str instanceof Array){
+        for (var i=0;i<str.length;i++){
+            if(str[i]==str[i+1]){
+                str.splice(i,1)
+                i=-1;
             }
-            compare=aStr[i];//B
-            newStr.push(compare);//['A','B']
-            break;
         }
-        istart=i+1;
+        return str ;
+    }else {
+        var newArry=str.split('');
+        for (var i=0;i<newArry.length;i++){
+                if(newArry[i]==newArry[i+1]){
+                    newArry.splice(i,1);
+                    i=-1;
+            }
+        }
+         return newArry;
     }
-   return newStr;
-};
-console.log(uniqueInOrder('AAAABBBCCDAABBB'));
+}
+uniqueInOrder('AAAABBBCCDAABBB');
+uniqueInOrder([0,1,1,2]);
 // var i=0;
 // do {
 //     i++;
--- kata/removeSmallest.js
+++ kata/removeSmallest.js
@@ -2,6 +2,9 @@
  * Created by 35031 on 2016/11/30.
  */
 function removeSmallest(numbers) {
+    if (numbers.length==0){
+        return [];
+    }else {
     var iMin=numbers[0];
     for (var i=1;i<numbers.length;i++){
             if (iMin>numbers[i]) iMin=numbers[i];
@@ -11,7 +14,6 @@ function removeSmallest(numbers) {
             numbers.splice(i,1);
             return numbers;
         }
-  }
-
+  }}
 };
-console.log(removeSmallest([8,2,3,4]));
+console.log(removeSmallest([]));

d9974d4eb9014ac847ed0fe9a69bf66645e2355b
wangpingo <350316275@qq.com>
2016-11-30 19:45:01 +0800

    更新代码

--- kata/removeSmallest.js
+++ kata/removeSmallest.js
@@ -2,5 +2,16 @@
  * Created by 35031 on 2016/11/30.
  */
 function removeSmallest(numbers) {
-    
-}
+    var iMin=numbers[0];
+    for (var i=1;i<numbers.length;i++){
+            if (iMin>numbers[i]) iMin=numbers[i];
+  }
+  for (var i=0;i<numbers.length;i++){
+        if (numbers[i]==iMin) {
+            numbers.splice(i,1);
+            return numbers;
+        }
+  }
+
+};
+console.log(removeSmallest([8,2,3,4]));

296da3424bb7ef42b9411e26ca3b2d0f1851bf2b
wangpingo <350316275@qq.com>
2016-12-01 22:27:31 +0800

    更新代码

--- kata/score.js
+++ kata/score.js
@@ -50,12 +50,9 @@ function score( dice ) {
     var dc = [0,0,0,0,0,0];
     var tdr = [1000,200,300,400,500,600];
     var sdr = [100,0,0,0,50,0];
-    dice.forEach(function(x){ dc[x-1]++; });
-
+    dice.forEach(function(x){dc[x-1]++;});
     return dc.reduce(function(s,x,i){
         return s + (x >= 3? tdr[i] : 0) + sdr[i]*(x % 3);
-    },0);
-
+    });
 }
-console.log(score([1, 1, 1, 3, 3]));
 

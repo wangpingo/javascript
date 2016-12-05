de355653e8efab5ad7b2d8374f5a43609bbe9528
wangpingo <350316275@qq.com>
2016-12-05 14:00:11 +0800

    更新代码

---
+++ kata/queueTime.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 35031 on 2016/12/5.
+ */
+function queueTime(customers,n) {
+
+}
+queueTime([],0);//0
+queueTime([1,2,3,4],1);//10
+queueTime([2,2,3,3,4,4],2);//9
+queueTime([1,2,3,4,5],100);//5

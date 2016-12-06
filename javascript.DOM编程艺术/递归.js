501f4ec6d6c7a753583c01b29870853c76380687
wangpingo <350316275@qq.com>
2016-12-06 15:33:15 +0800

    更新代码

--- javascript.DOM编程艺术/递归.js
+++ javascript.DOM编程艺术/递归.js
@@ -1,12 +1,18 @@
 /**
  * Created by 35031 on 2016/12/6.
  */
-function () {
-    
-}
-function digui(str) {
+var newArr=[];
+function digui(s,str) {
     for (var i=0;i<str.length;i++){
-        
+        s+=str[i];
+        str=str.replace(str[i],'');
+        if (str.length==0){
+            newArr.push(s);
+        }else {
+            digui(s,str);
+        }
+
     }
 } 
-digui('aab');
+digui('','ab');
+console.log(newArr);

a176de3141fad9ba506c2f42c696bff14eb0a5fe
wangpingo <350316275@qq.com>
2016-12-06 18:32:17 +0800

    更新代码

--- kata/permutations.js
+++ kata/permutations.js
@@ -3,39 +3,39 @@
  */
 // [1,2,3,4]
 // [a,a,b,b]
-function permutations(string) {
-    var newArr=[];
-    var str = string.split('').map((x,v)=>x=v+1);//[1,2,3,4]
-    var count = 0;
-    function arrange(s){
-        for(var i=0,length=str.length; i<length; i++) {
-            if(s.length == length - 1) {
-                if(s.indexOf(str[i]) < 0) {
-                    count++;
-                    s=s+str[i];
-                    var ns='';
-                    for (var i=0;i<s.length;i++){
-                        ns+=string[s[i]-1];
-                    }
-                    newArr.push(ns);
-                }
-                continue;
-            }
-            if(s.indexOf(str[i]) < 0) {
-                arrange(s+str[i]);
-            }
-        }
-    }
-    arrange("");
-    newArr.sort();
-    for (var i=0;i<newArr.length-1;i++){
-        if(newArr[i]==newArr[i+1]){
-            newArr.splice(i,1);
-            i=-1;
-        }
-    }
-    return newArr;
-}
+// function permutations(string) {
+//     var newArr=[];
+//     var str = string.split('').map((x,v)=>x=v+1);//[1,2,3,4]
+//     var count = 0;
+//     function arrange(s){
+//         for(var i=0,length=str.length; i<length; i++) {
+//             if(s.length == length - 1) {
+//                 if(s.indexOf(str[i]) < 0) {
+//                     count++;
+//                     s=s+str[i];
+//                     var ns='';
+//                     for (var i=0;i<s.length;i++){
+//                         ns+=string[s[i]-1];
+//                     }
+//                     newArr.push(ns);
+//                 }
+//                 continue;
+//             }
+//             if(s.indexOf(str[i]) < 0) {
+//                 arrange(s+str[i]);
+//             }
+//         }
+//     }
+//     arrange("");
+//     newArr.sort();
+//     for (var i=0;i<newArr.length-1;i++){
+//         if(newArr[i]==newArr[i+1]){
+//             newArr.splice(i,1);
+//             i=-1;
+//         }
+//     }
+//     return newArr;
+// }
 function permutations(string) {
     var arr = string.split(''), tmp = arr.slice(), heads = [], out = [];
     if(string.length == 1) return [string];
@@ -51,5 +51,12 @@ function permutations(string) {
 }
 // console.log(permutations('a')); // ['a'] 1
 // console.log(permutations('ab')); // ['ab', 'ba'] 12 21
-console.log(permutations('aabb')); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']1234 1324
+console.log(permutations('abad')); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']1234 1324
 //1234 1243 1324 1342 1423 1432  4*3*2
+// var arr=[1,2,3];
+// tmp = arr.slice().splice(0,1);
+// console.log(arr);
+// console.log(tmp);
+// var tem2=arr.splice(0,1);
+// console.log(arr);
+// console.log(tem2);

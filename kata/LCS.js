6c33f80c0370a2c2d5ce4f75678a79c4e0701b5c
wangpingo <350316275@qq.com>
2016-12-10 10:53:26 +0800

    更新代码

---
+++ kata/LCS.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 35031 on 2016/12/7.
+ */
+// LCS( "abcdef" , "abc" ) => returns "abc"
+// LCS( "abcdef" , "acf" ) => returns "acf"
+// LCS( "132535365" , "123456789" ) => returns "12356"
+function LCS(x,y) {
+
+}

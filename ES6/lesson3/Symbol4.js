5c3963acb39903d55320adfeef65d4c697181edb
wangpingo <350316275@qq.com>
2016-12-19 21:29:39 +0800

    更新代码

---
+++ ES6/lesson3/Symbol4.js
@@ -0,0 +1,33 @@
+/**
+ * Created by 35031 on 2016/12/19.
+ */
+'use strict'
+class UserGroup{
+    constructor(users){
+        //json {name:xxx}
+
+        this.users=users;
+    }
+    [Symbol.iterator](){
+        let i=0;
+        let self=this;
+        const names=Object.keys(this.users);
+        const length=names.length;
+        //iterator
+        return {
+            next:function () {
+                let name=names[i++];
+                let qq=self.users[name];
+                return {value: {name,qq},done:i>length}
+            }
+        }
+    }
+}
+let group=new UserGroup({'aaa':'1111','eee':'2222'});
+// for (let user of group){
+//     console.log(user)
+// }
+let  iterator= group[Symbol.iterator]();
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());

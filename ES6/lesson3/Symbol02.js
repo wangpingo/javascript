73706e30dacbfb708c043a299e5b711bbe289ea8
wangpingo <350316275@qq.com>
2016-12-19 20:21:30 +0800

    更新代码

--- ES6/lesson3/Symbol02.js
+++ ES6/lesson3/Symbol02.js
@@ -2,9 +2,10 @@
  * Created by 35031 on 2016/12/19.
  */
 'use strict';
+// let getName=2;
 const User=require('./User');
-// let getName=Symbol('getName');
-let getName=2;
+let getName=Symbol('getName');
+
 let user=new User();
 user.print();
 user[getName]();
---
+++ ES6/lesson3/symbols.js
@@ -0,0 +1,21 @@
+/**
+ * Created by 35031 on 2016/12/19.
+ */
+'use strict';
+let name=Symbol('name');
+let obj={
+    age:22,
+    [name]:'leo'
+};
+console.log(Object.keys(obj));
+for (let k in obj){
+    console.log(k);
+}
+for (let k=0;k<2;k++){
+
+}
+let k=1;
+if (k==1){
+    console.log(k)
+}
+console.log(k);

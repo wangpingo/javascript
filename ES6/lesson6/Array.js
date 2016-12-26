6f937611c561fe8f78a9860c5a8198b6c12206bb
wangpingo <350316275@qq.com>
2016-12-26 14:57:16 +0800

    更新代码

---
+++ ES6/lesson6/Array.js
@@ -0,0 +1,17 @@
+/**
+ * Created by 35031 on 2016/12/26.
+ */
+// Array.form      //类似数组的数据结构转换为数组
+// Array.of        //通过多个参数生成数组
+// [].copyWithin(target,start,end);  //通过自身数据，在指定位置 替换数据
+// [].find   [].findIndex()    //
+// [].fill
+    'use strict';
+var arr=Array.from({
+    '0':'qq 1593894520',
+    '1':'leo',
+    'length':2
+},function (item,index) {
+    return item+'----'+index;
+});
+console.log(arr);

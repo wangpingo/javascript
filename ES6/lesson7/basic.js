87dfdad1770a1b85217d4b94eb76ba56b30525c8
wangpingo <350316275@qq.com>
2016-12-29 23:59:30 +0800

    更新代码

---
+++ ES6/lesson7/basic.js
@@ -0,0 +1,8 @@
+/**
+ * Created by 35031 on 2016/12/29.
+ */
+var buffer=new ArrayBuffer(10);
+var arr=new Uint8Array(buffer);
+console.log(arr.length);
+var arr2=new Uint16Array(buffer);
+console.log(arr2.length);

68014c6d527193862c336734601239832211f2e6
wangpingo <350316275@qq.com>
2017-01-03 14:26:51 +0800

    更新代码

---
+++ ES6/lesson9/example.js
@@ -0,0 +1,5 @@
+/**
+ * Created by 35031 on 2017/1/3.
+ */
+export var name='leo';
+//exports.name='leo';
---
+++ ES6/lesson9/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by 35031 on 2017/1/3.
+ */
+// var name=require('./example').name;
+//console.log(name);

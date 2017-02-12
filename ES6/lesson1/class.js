b1cca7de249424e46cae581b8594ba9441b2c986
Administrator <350316275@qq.com>
2017-02-12 10:51:16 +0800

    更新代码

--- ES6/lesson1/class.js
+++ ES6/lesson1/class.js
@@ -61,6 +61,7 @@ class Manager extends User{
 var manager=new Manager('leo',22,'123');
 manager.changeName('zen ling');
 console.log(manager.name);
+
 console.log(manager.info);
 console.log(manager);
 // info 方法好用哈哈

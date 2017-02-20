fa79e932ecb944a52ad37fd84fe9d11c2f124df3
Administrator <350316275@qq.com>
2017-02-20 10:04:22 +0800

    更新代码

--- AMD和CMD/test.js
+++ AMD和CMD/test.js
@@ -2,8 +2,10 @@
  * Created by Administrator on 2017/2/19 0019.
  */
 // "use strict";
-require(["a"],function (arraySort) {
-    var a=['1','2','3','5'];
-    var a="aaa";
-    console.log(arraySort(a));
-});
+// require(["a"],function (arraySort) {
+//     var a=['1','2','3','5'];
+//     var a="aaa";
+//     console.log(arraySort(a));
+//基本概念
+// });
+console.log(11);
